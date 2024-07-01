784d92db35cff50da81af85bf526f9ff965a9609
tfjaervik <101225530+tfjaervik@users.noreply.github.com>
2024-07-01 13:30:14 +0200

    Update mnist.py
    changed keras.utils import and associated code

--- mnist.py
+++ mnist.py
@@ -1,6 +1,6 @@
 import numpy as np
 from keras.datasets import mnist
-from keras.utils import np_utils
+from keras.utils import to_categorical
 
 from dense import Dense
 from activations import Tanh
@@ -14,7 +14,7 @@ def preprocess_data(x, y, limit):
     x = x.astype("float32") / 255
     # encode output which is a number in range [0,9] into a vector of size 10
     # e.g. number 3 will become [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
-    y = np_utils.to_categorical(y)
+    y = to_categorical(y)
     y = y.reshape(y.shape[0], 10, 1)
     return x[:limit], y[:limit]
 
